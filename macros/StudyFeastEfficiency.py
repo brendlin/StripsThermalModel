436a4e5b700932fa35c4feb32a8b35ef03b9af3f
Claire David <claire.david@cern.ch>
2017-05-18 17:45:05 +0200

    Macro makeup and style

--- macros/StudyFeastEfficiency.py
+++ macros/StudyFeastEfficiency.py
@@ -38,21 +38,38 @@ def main(options,args) :
         if int(datapoint[1]) == 60 :
             data_60['x'].append(float(datapoint[0]))
             data_60['y'].append(float(datapoint[2]))
-            
+    
+    PlotUtils.ApplyGlobalStyle()
+    
+    # Drawing the fit 10 C + overlay data points:        
+    PoweringEfficiency.feast_fit_function_T10.SetLineColor(62) # blue
+    PoweringEfficiency.feast_fit_function_T10.Draw()    
+    
     feast_data_10 = ROOT.TGraph(len(data_10['x']),array('d',data_10['x']),array('d',data_10['y']))
     feast_data_10.SetMarkerSize(1)
-    feast_data_10.SetMarkerStyle(20)    
+    feast_data_10.SetMarkerStyle(20)
+    feast_data_10.SetMarkerColor(4)
+    feast_data_10.Draw('p same')
 
+    # Drawing the fit 60 C + overlay data points:
+    PoweringEfficiency.feast_fit_function_T60.SetLineColor(96)
+    PoweringEfficiency.feast_fit_function_T60.Draw('sames')    
     feast_data_60 = ROOT.TGraph(len(data_60['x']),array('d',data_60['x']),array('d',data_60['y']))
     feast_data_60.SetMarkerSize(1)
     feast_data_60.SetMarkerStyle(20)
-
-    PoweringEfficiency.feast_fit_function_T10.Draw()
-    PoweringEfficiency.feast_fit_function_T60.Draw('sames')
-    feast_data_10.Draw('p')
-    feast_data_60.Draw('p')
+    feast_data_60.SetMarkerColor(2)
+    feast_data_60.Draw('p same')
     
+    # Legending
+    leg= ROOT.TLegend(0.62, 0.65, 0.89, 0.87)
+    leg.AddEntry(feast_data_10, "Data, T = 10 C", "p")
+    leg.AddEntry(PoweringEfficiency.feast_fit_function_T10,"Fit, T = 10 C","l")
+    leg.AddEntry(feast_data_60, "Data, T = 60 C", "p")
+    leg.AddEntry(PoweringEfficiency.feast_fit_function_T60,"Fit, T = 60 C","l")
     
+    PlotUtils.SetStyleLegend(leg)
+    
+    leg.Draw()
     
     PlotUtils.SetStyleTitles(PoweringEfficiency.feast_fit_function_T10, "FEAST Efficiency fit", "I_{load} [A]", "FEAST Efficiency")
 
--- python/PlotUtils.py
+++ python/PlotUtils.py
@@ -5,6 +5,10 @@ import os,sys
 
 # To later defined gStyle common stuff for all plots
 
+# Does not seem to work...
+def ApplyGlobalStyle() :
+    ROOT.gStyle.SetMarkerSize(20)
+    ROOT.gStyle.SetMarkerSize(1)
 
 def SetStyleTitles(tobject, title, xtitle, ytitle) :
     
@@ -17,4 +21,11 @@ def SetStyleTitles(tobject, title, xtitle, ytitle) :
     tobject.GetXaxis().SetTitleOffset(1.3)
     tobject.GetYaxis().SetTitle(ytitle)
     tobject.GetYaxis().SetTitleOffset(1.3)
+    
+def SetStyleLegend(leg) :
+    
+    leg.SetBorderSize(0)
+    leg.SetFillColor(0)
+    leg.SetTextSize(0.035)
+    leg.SetTextFont(42)
     
