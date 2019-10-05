63b38dcff1ebd20f6cbdb9a3670cd6a4a1c0cb9b
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2019-10-05 08:00:44 +0000

    Fix sensor leakage bug in equation, affecting 700V
    (cherry picked from commit 62f3f8238d3abea1dcea8406bee207dff200bbc1)

--- python/SensorLeakage.py
+++ python/SensorLeakage.py
@@ -11,17 +11,26 @@ import SensorProperties
 # Leakage current as a function of flux
 # in A/cm2
 
-def iref(j) :
-    return max(0, (-0.14  + (3.40e-14) * j) / 1000000.)
+def iref500(j) :
+    return max(0, (-0.14  + (3.40e-14) * j) / 1000000.) # the 1e6 is for micro-amps to amps
 
 def iref700(j) :
-    return max(0, (-0.761 + (4.02e-14) * j) / 1000000.)
+    return max(0, (-0.761 + (4.02e-14) * j) / 1000000.) # the 1e6 is for micro-amps to amps
 
 def oldiref(j) :
-    return (1.22 + (2.68e-14) * j) / 1000000.
+    return (1.22 + (2.68e-14) * j) / 1000000. # the 1e6 is for micro-amps to amps
+
+if SensorProperties.vbias == 500 :
+    iref = iref500
+elif SensorProperties.vbias == 700 :
+    iref = iref700
+else :
+    print 'SensorLeakage.py has no knowledge of this voltage: %.0f'%(SensorProperties.vbias)
+    import sys; sys.exit()
 
 qref = []
 
 # size is nstep (???)
+# Sensor area is in cm2
 for i in range(GlobalSettings.nstep) :
     qref.append( SensorProperties.vbias * iref(OperationalProfiles.flux_list[i]*SensorProperties.area) )
